4717917e8f4a0f9b8aeac67772db04b561b57bb7
JeongbinYoon <yon980824@gmail.com>
2023-04-27 00:07:19 +0900

    POST 요청 글 생성

--- packages/blog-front/pages/manage/newpost.vue
+++ packages/blog-front/pages/manage/newpost.vue
@@ -1,38 +1,95 @@
 <template>
   <div>
-    <div class="editor">
-      <Editor />
+    <Editor v-model="content" class="editor" />
+
+    <div class="content">
+      <pre><code>{{ content }}</code></pre>
     </div>
-    <button @click="onSumbit">Submit</button>
+
+    <button @click="onSubmit">Submit</button>
   </div>
 </template>
 
 <script>
-// import { groq } from '@nuxtjs/sanity'
-import Editor from '@/components/Editor/index.vue'
-
-// const query = groq`*[_type == "category"]`
+import { createClient } from '@sanity/client'
+import Editor from '@/components/editor/Editor.vue'
 export default {
   components: {
     Editor,
   },
+
+  data() {
+    return {
+      content:
+        '<p>A Vue.js wrapper component for tiptap to use <code>v-model</code>.</p>',
+    }
+  },
   methods: {
-    onSumbit() {
-      // const response = await this.$sanity.fetch(query)
-      console.log('s')
+    onSubmit() {
+      const client = createClient({
+        projectId: 'lyvfm7vk',
+        dataset: 'production',
+        useCdn: false,
+        withCredentials: true,
+      })
+      const doc = {
+        _type: 'post',
+        id: 'ccccc',
+        title: 'My awesome blog post',
+        body: [
+          {
+            _type: 'block',
+            style: 'normal',
+            children: [{ _type: 'document', text: this.content }],
+          },
+        ],
+      }
+      client
+        .create(doc)
+        .then((response) =>
+          console.log(`Document was created, ID ${response._id}`)
+        )
+        .catch((error) => console.error('Error creating document:', error))
+
+      console.log(this.content)
     },
   },
 }
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
 .editor {
-  padding: 2rem;
-  p:focus {
-    outline: none;
+  padding: 20px;
+  ul,
+  ol {
+    padding-left: 40px;
+    margin: 16px 0;
   }
-  div:focus {
-    outline: none;
+}
+
+.ProseMirror {
+  padding: 20px;
+}
+
+.content {
+  padding: 1rem 0 0;
+
+  h3 {
+    margin: 1rem 0 0.5rem;
+  }
+
+  pre {
+    border-radius: 5px;
+    color: #333;
+  }
+
+  code {
+    display: block;
+    white-space: pre-wrap;
+    font-size: 0.8rem;
+    padding: 0.75rem 1rem;
+    background-color: #e9ecef;
+    color: #495057;
   }
 }
 </style>
